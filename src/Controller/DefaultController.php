cdde634bc1701645927966c52a2616a756efba10
Leandro Marchon <marchonleandro@gmail.com>
2018-11-13 11:35:05 -0200

    Trabalhando com sessões - V.1

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/default", name="default")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @param SessionInterface $session
+     * @Route("/pegar-sessao")
+     */
+    public function pegarSessao(SessionInterface $session)
+    {
+        echo $session->get('frase');
+        exit;
+    }
+}
